23515a6cc7166496cc72da6070307ad2e5918020
Miguel Oliveira Coelho <migueloliveiracoelho2003@gmail.com>
2026-06-10 13:14:44 -0300

    Implement leap year calculation logic

--- solutions/cobol/leap/1/src/leap.cob
+++ solutions/cobol/leap/1/src/leap.cob
@@ -3,9 +3,22 @@
        ENVIRONMENT DIVISION.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
+       01  WS-YEAR        PIC 9(4).
+       01  WS-RESULT      PIC 9.
        PROCEDURE DIVISION.
        LEAP.
-      * Enter solution here
+           MOVE 0 TO WS-RESULT
+           IF FUNCTION MOD(WS-YEAR, 4) = 0
+              MOVE 1 TO WS-RESULT
+              IF FUNCTION MOD(WS-YEAR, 100) = 0
+                 MOVE 0 TO WS-RESULT
+                 IF FUNCTION MOD(WS-YEAR, 400) = 0
+                    MOVE 1 TO WS-RESULT
+                 END-IF
+              END-IF
+           END-IF
+              
          CONTINUE.
        LEAP-EXIT.
          EXIT.
+         
